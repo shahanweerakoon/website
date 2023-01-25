1f9e652a26d88291640b38a416d6344002475452
shahanweerakoon <89102713+shahanweerakoon@users.noreply.github.com>
2023-01-25 14:41:34 +0530

    js

---
+++ user.js
@@ -0,0 +1,47 @@
+[
+  {
+    "name": "Shahan Weerakoon",
+    "city": "kleniya",
+    "image": "https://images.generated.photos/cfyS2JAPyD99zgYeMaYd60BF-rwtw-ZeBvJoGg1ROfY/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy8wMzQ1/MTE5LmpwZw.jpg"
+  },
+  {
+    "name": "Samn Perera",
+    "city": "colombo",
+    "image": "https://images.generated.photos/Z8LdiuBa_54Desj3PTDXrnFVvUOmM4LsfjRyFKM74Lw/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy8wNDQ3/MjYzLmpwZw.jpg"
+  },
+  {
+    "name": "Pahan Janaka",
+    "city": "colombo",
+    "image": "https://images.generated.photos/iIgX1cnnVtrerNosgrblT8y8ems_VU5i8oNME7t4rsE/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92Ml8w/NTkxODY5LmpwZw.jpg"
+  },
+  {
+    "name": "Maya Senevirathna",
+    "city": "colombo",
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6sGddmMZFZCqb7aJFx5eV-8FGj2gJWz7abGntj8IuyYdAv7W2HEJyi5WY3xbpLLzf-Zg&usqp=CAU"
+  },
+  {
+    "name": "pahan",
+    "city": "colombo",
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6sGddmMZFZCqb7aJFx5eV-8FGj2gJWz7abGntj8IuyYdAv7W2HEJyi5WY3xbpLLzf-Zg&usqp=CAU"
+  },
+  {
+    "name": "pahan",
+    "city": "colombo",
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6sGddmMZFZCqb7aJFx5eV-8FGj2gJWz7abGntj8IuyYdAv7W2HEJyi5WY3xbpLLzf-Zg&usqp=CAU"
+  },
+  {
+    "name": "pahan",
+    "city": "colombo",
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6sGddmMZFZCqb7aJFx5eV-8FGj2gJWz7abGntj8IuyYdAv7W2HEJyi5WY3xbpLLzf-Zg&usqp=CAU"
+  },
+  {
+    "name": "pahan",
+    "city": "colombo",
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6sGddmMZFZCqb7aJFx5eV-8FGj2gJWz7abGntj8IuyYdAv7W2HEJyi5WY3xbpLLzf-Zg&usqp=CAU"
+  },
+  {
+    "name": "pahan",
+    "city": "colombo",
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6sGddmMZFZCqb7aJFx5eV-8FGj2gJWz7abGntj8IuyYdAv7W2HEJyi5WY3xbpLLzf-Zg&usqp=CAU"
+  }
+]
